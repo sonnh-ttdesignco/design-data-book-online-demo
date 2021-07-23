75a7319886cc4e20e7e24dd1c39b0992a4ffbaff
SonNH <n.h.son@ttdesignco.com>
2021-07-23 13:44:12 +0700

    fix justify start

--- src/3d-cad/3d-cad-detail.js
+++ src/3d-cad/3d-cad-detail.js
@@ -1,2 +1,23 @@
 const pageName = "3d-cad";
 requiredLogin(pageName);
+
+function resizeBar() {
+    console.log('asdads')
+    let databookPageWidth = $(`.databook-page`).width();
+    $('#dragbar').mousedown(function (e) {
+        e.preventDefault();
+        $(document).mousemove(function (e) {
+            $('.table-content-wrap').css("width", e.pageX + 2);
+            $('.databook-wrap').css("width", databookPageWidth - (e.pageX + 2));
+        })
+    });
+
+    $(document).mouseup(function (e) {
+        $(document).unbind('mousemove');
+    });
+};
+
+
+$(function () {
+    resizeBar();
+})
